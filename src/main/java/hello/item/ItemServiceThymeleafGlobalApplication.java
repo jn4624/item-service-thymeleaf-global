01b276a0b814ac62aa01b296cf026e37965c4b09
LimJiNa <jn4624@gmail.com>
2024-03-16 22:13:47 +0900

    프로젝트명 및 설정 변경

--- src/main/java/hello/item/ItemServiceThymeleafGlobalApplication.java
+++ src/main/java/hello/item/ItemServiceThymeleafGlobalApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GlobalItemServiceByThymeleafApplication {
+public class ItemServiceThymeleafGlobalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GlobalItemServiceByThymeleafApplication.class, args);
+		SpringApplication.run(ItemServiceThymeleafGlobalApplication.class, args);
 	}
 
 }
