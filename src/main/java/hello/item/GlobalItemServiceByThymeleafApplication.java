6cd2ae5a2650fa2c1847cf6b6b79de25c2a61357
LimJiNa <jn4624@gmail.com>
2024-03-16 15:27:54 +0900

    Initial commit

---
+++ src/main/java/hello/item/GlobalItemServiceByThymeleafApplication.java
@@ -0,0 +1,13 @@
+package hello.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalItemServiceByThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalItemServiceByThymeleafApplication.class, args);
+	}
+
+}
